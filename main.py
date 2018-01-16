27cd3457890966201aa473931e2cbf286f5f802e
Andrew Madonna <andrew.w.madonna@gmail.com>
2018-01-16 12:31:33 -0500

    Working v2 transform

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
 import csv
+import sys
+import logging
+from copy import copy
 from decimal import Decimal
 from datetime import datetime
 from collections import OrderedDict
 
 import petl
 import click
+from smart_open import smart_open
 from passyunk.parser import PassyunkParser
+
+def get_logging():
+    logger = logging.getLogger()
+    handler = logging.StreamHandler(stream=sys.stderr)
+    formatter = logging.Formatter('[%(asctime)s] %(name)s %(levelname)s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    # def exception_handler(type, value, tb):
+    #     logger.exception("Uncaught exception: {0}".format(str(value)))
+
+    # sys.excepthook = exception_handler
+
+    return logger
+
+logger = None
+
+plates = None
+plates_counter = None
+ticket_numbers = None
+ticket_numbers_counter = None
+centriods = None
+dedup = False
+geocode_stats = {
+    'total': 0,
+    'success': 0,
+    'gps': 0,
+    'failed_address': 0,
+    'failed_segment': 0,
+    'failed_segments': set()
+}
 
 passyunk_parser = PassyunkParser()
 
@@ -54,10 +90,7 @@ fieldmap = OrderedDict([
         'type': 'string',
         'start_pos': 104,
         'end_pos': 112
-    })
-])
-
-latlon_fieldmap = OrderedDict([
+    }),
     ('lat', {
         'type': 'latlon',
         'start_pos': 112,
@@ -67,11 +100,13 @@ latlon_fieldmap = OrderedDict([
         'type': 'latlon',
         'start_pos': 122,
         'end_pos': 132
+    }),
+    ('gps', {
+        'type': 'boolean'
     })
 ])
 
 headers = list(fieldmap.keys())
-headers_with_latlon = list(fieldmap.keys() + latlon_fieldmap.keys())
 
 def parse_lat_lon(value):
     value = float(value)
@@ -94,17 +129,17 @@ typemap = {
 }
 
 def get_transform_row(latlon_input):
-    if latlon_input:
-        input_fieldmap = fieldmap + latlon_fieldmap
-    else:
-        input_fieldmap = fieldmap 
-
     def transform_row(line):
         row = OrderedDict()
-        for field in input_fieldmap.keys():
-            config = input_fieldmap[field]
-            row[field] = typemap[config['type']]\
-                            (line[0][config['start_pos']:config['end_pos']])
+        for field in fieldmap.keys():
+            config = fieldmap[field]
+            if config['type'] == 'latlon' and not latlon_input:
+                row[field] = None
+            elif 'start_pos' in config and 'end_pos' in config:
+                row[field] = typemap[config['type']]\
+                                (line[0][config['start_pos']:config['end_pos']])
+            else:
+                row[field] = None
 
         if not latlon_input:
             row['lat'] = None
@@ -123,12 +158,6 @@ def get_transform_row(latlon_input):
 
     return transform_row
 
-plates = None
-plates_counter = None
-ticket_numbers = None
-ticket_numbers_counter = None
-centriods = None
-
 def anonymize(row):
     global plates_counter, ticket_numbers_counter
 
@@ -136,109 +165,168 @@ def anonymize(row):
 
     plate_key = row[2] + row[3]
     if plate_key in plates:
-        out_row[3] = plates[plate_key]
+        out_row[3] = plates[plate_key]['id']
     else:
         plates_counter += 1
-        plates[plate_key] = plates_counter
+        plates[plate_key] = {
+            'id': plates_counter,
+            'plate': row[2],
+            'state': row[3],
+            'date_added': datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+        }
         out_row[3] = plates_counter
 
-    if row[0] in ticket_numbers:
-        out_row[0] = ticket_numbers[row[0]]
+    ticket_number = row[0]
+    if ticket_number in ticket_numbers:
+        if dedup is True:
+            ticket_numbers_counter += 1
+            ticket_numbers[ticket_number + 'E'] = ticket_numbers_counter
+            logger.info('Duplicate detected on ticket number {} - Adding {}E'.format(
+                ticket_number, ticket_number))
+        else:
+            out_row[0] = ticket_numbers[ticket_number]
     else:
         ticket_numbers_counter += 1
-        ticket_numbers[row[0]] = ticket_numbers_counter
+        ticket_numbers[ticket_number] = ticket_numbers_counter
         out_row[0] = ticket_numbers_counter
 
     return out_row
 
 def geocode(row):
+    global geocode_stats
+
     out_row = list(row)
 
     address_components = passyunk_parser.parse(row[5])
     out_row[5] = address_components['components']['output_address']
 
+    geocode_stats['total'] += 1
+
     ## No GPS lat/lon
-    if row[9] == None and address_components['components']['cl_seg_id'] != None:
-        if address_components['components']['cl_seg_id'] in centriods:
+    if row[9] == None:
+        if address_components['components']['cl_seg_id'] != None and \
+           address_components['components']['cl_seg_id'] in centriods:
             lat_lon = centriods[address_components['components']['cl_seg_id']]
-            out_row[9] = lat_lon[0]
-            out_row[10] = lat_lon[1]
-            row[10] = False
+            out_row[9] = lat_lon['Lat']
+            out_row[10] = lat_lon['lon']
+            out_row[11] = False
+            geocode_stats['success'] += 1
         else:
-            ## TODO: !!!
-            print('!!! {} not found'.format(address_components['components']['cl_seg_id']))
+            if address_components['components']['cl_seg_id'] != None:
+                geocode_stats['failed_segment'] += 1
+                segment = address_components['components']['cl_seg_id']
+            else:
+                geocode_stats['failed_address'] += 1
+                segment = None
+
+            if segment:
+                geocode_stats['failed_segments'].add(segment)
+            logger.info('Geocode - {} not found'.format(segment or row[5]))
     else:
-        row[10] = True
+        geocode_stats['gps'] += 1
+        out_row[10] = True
 
     return out_row
 
-def load_index_file(path):
+def load_index_file(path, _type):
     if path == None:
         return {}
 
-    ## TODO: S3 support
-
-    ## TODO: !!! plate has multiple cols
-
-    with open(path) as file:
-        reader = csv.reader(file)
-        next(reader) # skip headers
+    with smart_open(path, 'r') as file:
+        reader = csv.DictReader(file)
 
         index = {}
         for row in reader:
-            if len(row) > 2:
-                index[row[0]] = row[1:]
+            if _type == 'plates_file':
+                index[row['state'] + row['plate']] = row
+            elif _type == 'license_file':
+                index[row['ticket_number']] = row['anon_ticket_number']
+            elif _type == 'centriod_file':
+                index[row['SEG_ID']] = row
             else:
-                index[row[0]] = row[1]
+                raise Exception('`{}` not a supported index file type'.format(_type))
 
         return index
 
-def save_index_file(path, index, headers):
-    with open(path) as file:
-        writer = csv.writer(file)
+def save_index_file(path, index, headers, _type):
+    if path:
+        with smart_open(path, 'w') as file:
+            writer = csv.DictWriter(file, fieldnames=headers)
 
-        writer.write(headers)
+            writer.writeheader()
 
-        for key, value in index.items():
-            if isinstance(value, list):
-                writer.write([key] + value)
-            else:
-                writer.write([key] + [value])
+            for key, value in index.items():
+                if _type == 'plates_file':
+                    writer.writerow(value)
+                elif _type == 'license_file':
+                    writer.writerow({'ticket_number': key, 'anon_ticket_number': value})
+                else:
+                    raise Exception('`{}` not a supported index file type'.format(_type))
 
 @click.command()
 @click.option('--plates-file')
 @click.option('--ticket-numbers-file')
 @click.option('--centriod-file')
 @click.option('--latlon-input/--no-latlon-input', is_flag=True, default=True)
-def main(plates_file, ticket_numbers_file, centriod_file, latlon_input):
-    global plates, plates_counter, ticket_numbers, ticket_numbers_counter, centriods
+@click.option('--deduplicate/--no-deduplicate', is_flag=True, default=False, help="Add 'E' to the end of duplicate ticket numbers")
+def main(plates_file, ticket_numbers_file, centriod_file, latlon_input, deduplicate):
+    global logger, plates, plates_counter, ticket_numbers, ticket_numbers_counter, centriods, dedup
 
-    plates = load_index_file(plates_file)
+    logger = get_logging()
+
+    dedup = deduplicate
+
+    plates = load_index_file(plates_file, 'plates_file')
     plate_numbers_values = plates.values()
     if len(plate_numbers_values) > 0:
-        plates_counter = max(plate_numbers_values)
+        plates_counter = max(map(lambda x: int(x['id']), plate_numbers_values))
     else:
         plates_counter = 0
 
-    ticket_numbers = load_index_file(ticket_numbers_file)
+    logger.info('Plates autoincrement starting at: {}'.format(plates_counter))
+
+    ticket_numbers = load_index_file(ticket_numbers_file, 'license_file')
     ticket_numbers_values = ticket_numbers.values()
     if len(ticket_numbers_values) > 0:
-        ticket_numbers_counter = max(ticket_numbers_values)
+        ticket_numbers_counter = max(map(lambda x: int(x), ticket_numbers_values))
     else:
         ticket_numbers_counter = 0
 
-    centriods = load_index_file(centriod_file)
+    logger.info('Ticket number autoincrement starting at: {}'.format(ticket_numbers_counter))
 
-    petl\
-        .fromtext(strip=False)\
-        .rowmap(get_transform_row(latlon_input), header=headers_with_latlon, failonerror=True)\
-        .select('{fine} > 0.0')\
-        .rowmap(anonymize, header=headers_with_latlon, failonerror=True)\
-        .rowmap(geocode, header=headers_with_latlon + ['gps'], failonerror=True)\
+    centriods = load_index_file(centriod_file, 'centriod_file')
+
+    (
+        petl
+        .fromtext(strip=False)
+        .rowmap(get_transform_row(latlon_input), header=headers, failonerror=True)
+        .select('{fine} > 0.0')
+        .rowmap(anonymize, header=headers, failonerror=True)
+        .rowmap(geocode, header=headers + ['gps'], failonerror=True)
         .tocsv()
+    )
 
-    save_index_file(plates_file, plates, ['plate_id', 'plate'])
-    save_index_file(ticket_numbers_file, ticket_numbers)
+    logger.info('Geocode stats - success rate: {:.2%}, successes: {}, gps: {}, failed_segment: {}, failed_address: {}'.format(
+        geocode_stats['success'] / geocode_stats['total'],
+        geocode_stats['success'],
+        geocode_stats['gps'],
+        geocode_stats['failed_segment'],
+        geocode_stats['failed_address']))
+
+    if len(geocode_stats['failed_segments']) > 0:
+        logger.info('Failed Segments - {}'.format(','.join(geocode_stats['failed_segments'])))
+
+    save_index_file(plates_file,
+                    plates,
+                    ['id',
+                     'plate',
+                     'state',
+                     'date_added'],
+                    'plates_file')
+    save_index_file(ticket_numbers_file,
+                    ticket_numbers,
+                    ['ticket_number', 'anon_ticket_number'],
+                    'license_file')
 
 if __name__ == '__main__':
     main()
